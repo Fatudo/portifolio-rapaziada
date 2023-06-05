1fbd347b28e181644ac487cfe01c0b89732577c5
Lucas Fatudo Junqueira <lucasfatudojunqueira@gmail.com>
2023-06-05 00:16:31 -0300

    update

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + './public/index.html');
+
+});
+
+app.listen(port, () => {
+    console.log('Servidor web rodando em http://localhost:3000');
+});
